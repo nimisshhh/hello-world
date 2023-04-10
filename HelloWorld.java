fa18db3014fe537a9225c47214c258d5677af7a5
nimisshhh <sharma_nimish@hotmail.com>
2023-04-10 18:54:04 -0400

    Second Commit with For Loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld
 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args)
+    {
+        for(int i = 0; i <5; i++)
+        {
+            System.out.println( (i + 1) + " Hello World");
+        }
     }
 }
 
